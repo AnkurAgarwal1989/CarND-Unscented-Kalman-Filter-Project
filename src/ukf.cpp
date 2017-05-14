b0dd5c9a284d2b8e005fcae93b17de787d29a88b
Ankur Agarwal <ankuraga@gmail.com>
2017-05-14 00:59:34 -0700

    Working set of values for synth data

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -63,10 +63,10 @@ UKF::UKF() {
   //cout << weights_;
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 0.2;  //1.0      //data1:0.3 //data2: 0.15
+  std_a_ = 0.3;  //1.0      //data:0.3
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 1.0; //10.0  //data1:0.8 //data2: 0.01
+  std_yawdd_ = 1.15; //10.0  //data:1.15
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
